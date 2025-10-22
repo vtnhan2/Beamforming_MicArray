daa2b7f3c96267472d937c24068aa57b51c73e28
ThanhNhan <vothanhnhan211@gmail.com>
2025-10-22 09:56:15 +0700

    choke: delete old file wav

---
+++ sca.c
@@ -0,0 +1,11 @@
+c
+    module
+    Bai1(
+        input
+            input);
+[0:0] CLOCK_50,
+    KEY
+        system NIOS_system(
+                .clk_clk(CLOCK_50),
+                .reset_reset_n(KEY[0]));
+endmodule
